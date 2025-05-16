662108ef740fc33bcbaaad5165f76b4029ee49de
Nimbus869 <jett869@gmail.com>
2025-05-15 23:01:36 -0500

    setup display window

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ import pygame
 from constants import *
 
 class Main():
-    pass
+    pygame.init()
+    display_surface = pygame.display.set_mode(size=(WINDOW_WIDTH,WINDOW_HIGHT))
+    pygame.display.set_caption("My Pong")
+    is_running = True
+    while is_running:
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                is_running = False
+        display_surface.fill("blue")
+        pygame.display.update()
+    pygame.quit()
